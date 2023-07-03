e4cf66598f2cca104c740ca6ec424cc83872fe22
Ashley Wulber <ashley@system76.com>
2023-07-03 10:18:00 -0400

    fix: improve logging

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,24 @@ mod localize;
 mod subscriptions;
 mod util;
 use config::APP_ID;
-use tracing::info;
+use tracing::{info, metadata::LevelFilter};
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 use localize::localize;
 
 use crate::config::VERSION;
 
-fn main() -> cosmic::iced::Result {
-    // Initialize logger
-    if std::env::var("TOKIO_CONSOLE").as_deref() == Ok("1") {
-        std::env::set_var("RUST_LOG", "trace");
-        console_subscriber::init();
-    } else {
-        tracing_subscriber::fmt::init();
-    }
+fn main() -> anyhow::Result<()> {
+    tracing_subscriber::registry()
+        .with(tracing_journald::layer()?)
+        .with(fmt::layer())
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .try_init()?;
+    log_panics::init();
 
     info!("cosmic-notifications ({})", APP_ID);
     info!("Version: {} ({})", VERSION, config::profile());
@@ -25,5 +29,6 @@ fn main() -> cosmic::iced::Result {
     // Prepare i18n
     localize();
 
-    app::run()
+    app::run()?;
+    Ok(())
 }
