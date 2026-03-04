9ebd110c3e9f13846988a55faf9ed40cdbdce0a7
Josh Megnauth <jo.sh@tutanota.com>
2026-03-04 14:03:16 -0500

    Store "sender-pid" and fix Clippy lints
    I bundled the Clippy fixes with this commit since they're small and
    don't warrant its own PR.
    
    "sender-pid" is a hint set by some apps, such as Electron apps or
    "notify-send." It's useful as a more granular indicator than app ID
    because it's the PID of the sending app.
    
    For COSMIC Notifications, this commit fixes a warning but does not use
    "sender-pid" directly. It's stored for further use like the other hints.

--- cosmic-notifications-util/src/lib.rs
+++ cosmic-notifications-util/src/lib.rs
@@ -6,7 +6,8 @@ pub use image::*;
 use cosmic::widget::{Icon, icon};
 use serde::{Deserialize, Serialize};
 use std::{
-    collections::HashMap, convert::Infallible, fmt, path::PathBuf, str::FromStr, time::SystemTime,
+    collections::HashMap, convert::Infallible, fmt, num::NonZeroU32, path::PathBuf, str::FromStr,
+    time::SystemTime,
 };
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
@@ -47,6 +48,10 @@ impl Notification {
                 "category" => String::try_from(v).map(Hint::Category).ok(),
                 "desktop-entry" => String::try_from(v).map(Hint::DesktopEntry).ok(),
                 "resident" => bool::try_from(v).map(Hint::Resident).ok(),
+                "sender-pid" => u32::try_from(v)
+                    .ok()
+                    .and_then(NonZeroU32::new)
+                    .map(Hint::SenderPid),
                 "sound-file" => String::try_from(v)
                     .map(|s| Hint::SoundFile(PathBuf::from(s)))
                     .ok(),
@@ -105,7 +110,7 @@ impl Notification {
     }
 
     pub fn transient(&self) -> bool {
-        self.hints.iter().any(|h| *h == Hint::Transient(true))
+        self.hints.contains(&Hint::Transient(true))
     }
 
     pub fn category(&self) -> Option<&str> {
@@ -135,6 +140,13 @@ impl Notification {
     pub fn image(&self) -> Option<&Image> {
         self.hints.iter().find_map(|h| match h {
             Hint::Image(i) => Some(i),
+            _ => None,
+        })
+    }
+
+    pub fn sender_pid(&self) -> Option<NonZeroU32> {
+        self.hints.iter().find_map(|h| match h {
+            Hint::SenderPid(pid) => Some(*pid),
             _ => None,
         })
     }
@@ -206,6 +218,7 @@ pub enum Hint {
     Image(Image),
     IconData(Vec<u8>),
     Resident(bool),
+    SenderPid(NonZeroU32),
     SoundFile(PathBuf),
     SoundName(String),
     SuppressSound(bool),
@@ -216,7 +229,6 @@ pub enum Hint {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
-
 pub enum Image {
     Name(String),
     File(PathBuf),
